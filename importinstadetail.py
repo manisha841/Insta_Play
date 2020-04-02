accd73bfab86e7e03b77cb46e671a4f4f42b9126
Manisha <35535019+manisha841@users.noreply.github.com>
2020-04-02 20:34:45 +0545

    Update and rename importinstadp.py to importinstadetail.py

--- importinstadetail.py
+++ importinstadetail.py
@@ -2,7 +2,7 @@ import instaloader as insta
 profile=insta.Instaloader()
 a=input("Enter the username:")
 profile.download_profile(a,profile_pic_only=True)
-#profile.download_profile(a,profile_pic=true)
+
 
 profile.download_profile(a,download_tagged_only=True)
 profile.download_saved_posts(a,download_saved_posts=True)
